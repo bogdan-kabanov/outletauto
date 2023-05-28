ef03ee6bde6ef251ca2cb6b82ffe6bec5c13441f
Богдан Кабанов <121943899+BohdanKabanovProf@users.noreply.github.com>
2023-05-28 13:54:20 +0300

    Сайт - OutletAuto | Правки 1.0 | Версия 1.0
    Верстка главной страницы

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+const searchHeader = document.querySelector("#header-search");
+window.addEventListener("resize", function () {
+  searchHeader.setAttribute(
+    "placeholder",
+    this.innerWidth >= 768 ? "Поиск по артикулу или VIN коду" : "Артикул"
+  );
+});
+window.dispatchEvent(new Event("resize"));
+
+// Анимация стрелочки на лицевом блоке сайта
+const fixedArrow = document.querySelectorAll('.arrow-fixed .arrow');
+setInterval(()=>{
+  fixedArrow.forEach(e=>{
+    e.classList.toggle('arrow-animate');
+  })
+}, 800*5)
