3f66265ca2d8260e3f1d460e58aa28d778cfdb57
Daniil <77277774+shamemask@users.noreply.github.com>
2023-07-14 22:29:09 +0300

    завершил регистрацию и отправку подтвержения на почту

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -7,12 +7,37 @@ btn.forEach(item => {
     item.onclick = function() {
     modal.style.display = "block";
     }
-    span.onclick = function() {
-    modal.style.display = "none";
-    }
+//    span.onclick = function() {
+//    modal.style.display = "none";
+//    }
     window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
     }
 })
+
+function registerUser(button) {
+  var formNode = button.parentNode.parentNode
+  var form = $('#' + formNode.id); // Идентификатор вашей формы регистрации
+  var url = form.attr('action');
+  var formData = form.serialize();
+
+  $.ajax({
+    url: url,
+    type: 'POST',
+    data: formData,
+    success: function(response) {
+      // Обработка успешного ответа от сервера
+      // Например, закрытие модального окна и обновление страницы
+      $('#registration-modal').modal('hide');
+      location.reload();
+    },
+    error: function(xhr) {
+      // Обработка ошибки
+      // Например, вывод сообщения об ошибке в модальном окне
+      var errorMessage = xhr.responseText;
+      $('#registration-error-message').text(errorMessage);
+    }
+  });
+}
