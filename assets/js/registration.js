8c460d0d068bfe5f8cb079143d3fb64e3af34e7c
Daniil <77277774+shamemask@users.noreply.github.com>
2023-09-18 20:44:57 +0300

    подвязал страницы

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -1,16 +1,51 @@
 // Скрипт для открытия и закрытия модального окна
 
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
+var RegModal = document.getElementById("RegModal");
+var RegBtn = document.querySelectorAll("#RegBtn");
 var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-modal.style.display = "block";
-}
-span.onclick = function() {
-modal.style.display = "none";
-}
+var AuthModal = document.getElementById("AuthModal");
+var LogBtn = document.querySelectorAll("#LogBtn");
+RegBtn.forEach(item => {
+    item.onclick = function() {
+    RegModal.style.display = "block";
+    }
+
+})
+LogBtn.forEach(item => {
+    item.onclick = function() {
+    AuthModal.style.display = "block";
+    }
+})
 window.onclick = function(event) {
-if (event.target == modal) {
-  modal.style.display = "none";
+    if (event.target == RegModal) {
+      RegModal.style.display = "none";
+    };
+    if (event.target == AuthModal) {
+      AuthModal.style.display = "none";
+    }
 }
+
+function registerUser(button) {
+  var formNode = button.parentNode.parentNode
+  var form = $('#' + formNode.id); // Идентификатор вашей формы регистрации
+  var url = form.attr('action');
+  var formData = form.serialize();
+
+  $.ajax({
+    url: url,
+    type: 'POST',
+    data: formData,
+    success: function(response) {
+      // Обработка успешного ответа от сервера
+      // Например, закрытие модального окна и обновление страницы
+      $('#registration-modal').modal('hide');
+      location.reload();
+    },
+    error: function(xhr) {
+      // Обработка ошибки
+      // Например, вывод сообщения об ошибке в модальном окне
+      var errorMessage = xhr.responseText;
+      $('#registration-error-message').text(errorMessage);
+    }
+  });
 }
